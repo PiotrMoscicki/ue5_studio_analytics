126a3e12f02bb83f68b50f80f5da322de46c31a8
Piotr Mościcki / squares <m.piotr@moscicki.pl>
2022-12-27 18:20:41 +0000

    Remove static and media folders and related features

--- Server/services/web/project/__init__.py
+++ Server/services/web/project/__init__.py
@@ -3,12 +3,10 @@ import os
 from flask import (
     Flask,
     jsonify,
-    send_from_directory,
     request,
     Response,
 )
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.utils import secure_filename
 from datetime import datetime, timezone
 
 
@@ -49,27 +47,3 @@ def studio_analytics_v1() -> Response:
             db.session.commit()
 
     return Response("{}", status=201, mimetype="application/json")
-
-@app.route("/static/<path:filename>")
-def staticfiles(filename):
-    return send_from_directory(app.config["STATIC_FOLDER"], filename)
-
-
-@app.route("/media/<path:filename>")
-def mediafiles(filename):
-    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
-
-
-@app.route("/upload", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        file = request.files["file"]
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
-    return """
-    <!doctype html>
-    <title>upload new File</title>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file><input type=submit value=Upload>
-    </form>
-    """
